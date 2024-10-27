c083e3e08f04e3f441d0720bc78e35ec428efff6
Isaias silva <Isaiasprog@outlook.com>
2024-10-27 00:02:00 -0300

    Foi ajustada as imagem e adicionado um espaço para o instagram

--- scripts.js
+++ scripts.js
@@ -1,29 +1,28 @@
-// Funções para abrir o WhatsApp com a mensagem de interesse no ebook
-function comprarEbook1() {
-    const whatsappNumber = "558488770810";
-    const text = "Olá, tenho interesse em comprar o ebook1 de canto e instrumentos musicais.";
-    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
-    window.open(whatsappUrl, "_blank");
+// Função para redirecionar ao link do e-book
+function abrirLinkEbook() {
+    const ebookUrl = "https://seulinkdoebook.com"; // Insira o link do e-book aqui
+    window.open(ebookUrl, "_blank");
 }
 
-function comprarEbook2() {
-    const whatsappNumber = "558488770810";
-    const text = "Olá, tenho interesse em comprar o ebook2 de canto e instrumentos musicais.";
-    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
-    window.open(whatsappUrl, "_blank");
+function comprarEbook() {
+    window.location.href = 'https://pay.cakto.com.br/5DjLy4c?fbclid=PAZXh0bgNhZW0CMTEAAabOgIZLA-Ljvk-A1TZXCar4JoqIgHkfiX0Ksuz1fnohuG7XgzhRHeP4d2U_aem_fRRC5nT0eJISADXNYb-Msw';
 }
 
-function comprarEbook3() {
-    const whatsappNumber = "558488770810";
-    const text = "Olá, tenho interesse em comprar o ebook3 de canto e instrumentos musicais.";
-    const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
-    window.open(whatsappUrl, "_blank");
-}
+// Função para redirecionar ao WhatsApp com a mensagem de interesse no e-book
+// function comprarEbook() {
+//     const whatsappNumber = "558488770810";
+//     const text = "Olá, tenho interesse em comprar o ebook de canto e instrumentos musicais.";
+//     const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
+//     window.open(whatsappUrl, "_blank");
+// }
 
-// Função para redirecionar para o WhatsApp com a mensagem de agendamento de aula
+// Função para redirecionar ao WhatsApp com a mensagem de agendamento de aula
 function agendarAula() {
     const mensagem = "Gostaria de agendar uma aula!";
     window.open(`https://wa.me/558488770810?text=${encodeURIComponent(mensagem)}`, '_blank');
+}
+function insta() {
+    window.location.href = "https://www.instagram.com/stories/highlights/17974580978724789/";
 }
 
 // Função que envia os dados do formulário para o WhatsApp
@@ -34,24 +33,21 @@ document.querySelector("form").addEventListener("submit", function (event) {
     const email = document.getElementById("email").value;
     const mensagem = document.getElementById("mensagem").value;
 
-    const whatsappNumber = "558488770810"; // Número de WhatsApp de destino
+    const whatsappNumber = "558488770810";
     const text = `Olá, meu nome é ${nome}. Meu email é ${email}. Gostaria de saber mais informações: ${mensagem}`;
-
-    // Cria o link do WhatsApp com os parâmetros do texto
     const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(text)}`;
 
-    // Redireciona o usuário para o WhatsApp com a mensagem personalizada
     window.open(whatsappUrl, "_blank");
 });
 
-// Atribuindo os eventos de clique aos botões
-// document.querySelector('.btn-ebook').addEventListener('click', comprarEbook);
+// Atribuindo os eventos de clique aos botões de e-book e de aula
+document.querySelector('.btn-ebook-link').addEventListener('click', abrirLinkEbook);
+document.querySelector('.btn-ebook-compra').addEventListener('click', comprarEbook);
 document.querySelector('.btn-aula').addEventListener('click', agendarAula);
 
 // Função que aplica a classe "visible" aos elementos ao rolar a página
 function handleScroll() {
     const elements = document.querySelectorAll('.hidden');
-
     elements.forEach((el) => {
         const rect = el.getBoundingClientRect();
         if (rect.top <= window.innerHeight * 0.85) {
